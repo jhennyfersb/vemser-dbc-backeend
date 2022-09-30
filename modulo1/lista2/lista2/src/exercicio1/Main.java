3a447540f908d77726fdf3d27f736d121b22c60f
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2022-09-30 10:31:18 -0300

    lista 2

--- modulo1/lista2/lista2/src/exercicio1/Main.java
+++ modulo1/lista2/lista2/src/exercicio1/Main.java
@@ -1,2 +1,22 @@
-package exercicio1;public class Main {
+package exercicio1;
+
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Digite o nome do produto");
+        String nomeProduto = sc.next();
+        System.out.println("Digite o preço do produto");
+        double valorProduto = sc.nextDouble();
+
+        System.out.println("Produto: " + nomeProduto);
+        System.out.println("Preço R$: " + valorProduto);
+        System.out.println("Promoção: " + nomeProduto);
+        for (int i = 1; i <= 10; i++) {
+            double desconto = valorProduto - (valorProduto * (0.05 * i));
+            System.out.format(i + " x " + "R$ %.2f", desconto);
+            System.out.format(" = R$ %.2f\n", i * desconto);
+        }
+    }
 }
--- modulo1/lista2/lista2/src/exercicio2/Main.java
+++ modulo1/lista2/lista2/src/exercicio2/Main.java
@@ -1,10 +1,11 @@
-package exercico2;
+package exercicio2;
 
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
+        System.out.println("Digite um numero");
         int numeroASerAdivinhado = sc.nextInt();
 
         while (true){
