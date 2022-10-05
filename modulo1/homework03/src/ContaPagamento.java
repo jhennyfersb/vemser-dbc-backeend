2e521f57c88920cc13efdd27ac99f5c6439500bd
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2022-10-05 15:01:55 -0300

    Correção homework 03 Conta Corrente

--- modulo1/homework03/src/ContaPagamento.java
+++ modulo1/homework03/src/ContaPagamento.java
@@ -7,11 +7,18 @@ public class ContaPagamento extends Conta implements Impressao {
     public ContaPagamento(Cliente cliente, String numeroConta, String agencia, double saldo) {
         super(cliente, numeroConta, agencia, saldo);
     }
-
     @Override
     public boolean sacar(double valorSacar) {
         if (getSaldo() >= valorSacar + TAXA_SAQUE) {
             super.setSaldo(getSaldo() - valorSacar - TAXA_SAQUE);
+            return true;
+        }
+        return false;
+    }
+    @Override
+    public boolean transferir(Conta conta, double valorTransferir) {
+        if (sacar(valorTransferir)) {
+            conta.depositar(valorTransferir);
             return true;
         }
         return false;
@@ -25,7 +32,7 @@ public class ContaPagamento extends Conta implements Impressao {
                 "\nCliente : " + getCliente().getNome());
         System.out.println("Numero da Conta : " + getNumeroConta() + "\nAgência : " + getAgencia());
         System.out.println("Saldo antes de Sacar : " + df.format(saldoAntesDeSacar));
-        System.out.println("Valor do Saque : " + df.format(saldoAntesDeSacar - getSaldo()));
+        System.out.println("Valor do Saque : " + df.format(saldoAntesDeSacar - getSaldo() - TAXA_SAQUE));
         System.out.println("Taxa de saque : " + df.format(TAXA_SAQUE) + "\nSaldo Atual : " + df.format(getSaldo()));
         System.out.println("-------------------------------------");
     }
