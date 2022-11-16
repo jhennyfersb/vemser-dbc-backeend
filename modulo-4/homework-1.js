dad3692cf0536e1ecd3c0980202d5f7e93d60ca6
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2022-11-16 09:03:12 -0300

    homework 1

--- modulo-4/homework-1.js
+++ modulo-4/homework-1.js
@@ -25,16 +25,20 @@ db.alunos.insert(
 	"data_nascimento" :new Date(1995,12,06)
     }
 )
-
+//find alunos
 db.alunos.find({})
 
+//find aluno
 db.alunos.find(
     { 
       "nome" : "Jhennyfer Sobrinho"
     }
     ).pretty()
-    
+
     db.alunos.find({
         "nome" : "Cassia Sobrinho"
     }
     ).pretty()   
+
+//find data_nascimento
+    db.alunos.find({"data_nascimento" :{$eq :new Date('1996-01-06T02:00:00.000Z')}})
