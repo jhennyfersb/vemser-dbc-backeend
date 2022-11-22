043b00044038b90e333d7ab4cbe88062eb61a540
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2022-11-22 09:18:06 -0300

    stack 1

--- modulo-4.3/produto-consumidor/src/main/java/dbc/company/produtoconsumidor/controller/ProdutorController.java
+++ modulo-4.3/produto-consumidor/src/main/java/dbc/company/produtoconsumidor/controller/ProdutorController.java
@@ -1,6 +1,9 @@
 package dbc.company.produtoconsumidor.controller;
 
 import dbc.company.produtoconsumidor.service.ProdutorService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +16,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProdutorController {
     private final ProdutorService produtorService;
 
+    @Operation(summary = "Recebe mensagem", description = "insere mensagem no tópico")
+    @ApiResponses(
+            value = {
+                    @ApiResponse(responseCode = "200", description = "Retorna mensagem no tópico"),
+                    @ApiResponse(responseCode = "403", description = "Você não tem permissão para acessar este recurso"),
+                    @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
+            }
+    )
     @PostMapping("/enviar-mensagem")
     public void enviarmensagem(String mensagem){
         produtorService.enviarMensagem(mensagem);
