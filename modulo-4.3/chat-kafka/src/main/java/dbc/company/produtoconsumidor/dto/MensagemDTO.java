2359fbd7b90577857f7aca855725fb16f5be07f6
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2022-11-23 10:33:13 -0300

    stack 2

---
+++ modulo-4.3/chat-kafka/src/main/java/dbc/company/produtoconsumidor/dto/MensagemDTO.java
@@ -0,0 +1,15 @@
+package dbc.company.produtoconsumidor.dto;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class MensagemDTO {
+    private String usuario;
+    private String mensagem;
+    private LocalDateTime dataCriacao;
+}
