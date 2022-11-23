55eaa1c5aa471dc93735d77b54dada85d989847b
Jhennyfer Sobrinho <103534079+jhennyfersb@users.noreply.github.com>
2022-11-23 10:32:05 -0300

    stack 2

--- modulo-4.3/produto-consumidor/src/main/java/dbc/company/produtoconsumidor/controller/ProdutorController.java
+++ modulo-4.3/produto-consumidor/src/main/java/dbc/company/produtoconsumidor/controller/ProdutorController.java
@@ -1,14 +1,14 @@
 package dbc.company.produtoconsumidor.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dbc.company.produtoconsumidor.dto.MensagemDTO;
+import dbc.company.produtoconsumidor.dto.NomeChat;
 import dbc.company.produtoconsumidor.service.ProdutorService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/kafka")
@@ -24,8 +24,9 @@ public class ProdutorController {
                     @ApiResponse(responseCode = "500", description = "Foi gerada uma exceção")
             }
     )
-    @PostMapping("/enviar-mensagem")
-    public void enviarmensagem(String mensagem){
-        produtorService.enviarMensagem(mensagem);
+    @PostMapping("/enviar-mensagem{destinatario}")
+    public void enviarmensagem(@PathVariable("destinatario") NomeChat destinatario,
+                               @RequestBody MensagemDTO mensagem) throws JsonProcessingException {
+        produtorService.enviarMensagem(mensagem,destinatario);
     }
 }
--- modulo-4.3/produto-consumidor/src/main/java/dbc/company/produtoconsumidor/service/ProdutorService.java
+++ modulo-4.3/produto-consumidor/src/main/java/dbc/company/produtoconsumidor/service/ProdutorService.java
@@ -1,13 +1,20 @@
 package dbc.company.produtoconsumidor.service;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import dbc.company.produtoconsumidor.dto.MensagemDTO;
+import dbc.company.produtoconsumidor.dto.NomeChat;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.kafka.support.KafkaHeaders;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.util.concurrent.ListenableFutureCallback;
 
 import java.util.UUID;
 
@@ -15,16 +22,34 @@ import java.util.UUID;
 @RequiredArgsConstructor
 @Slf4j
 public class ProdutorService {
-    @Value(value = "${kafka.topic}")
-    private String topic;
+//    @Value(value = "${kafka.topic}")
+//    private String topic;
 
-    private final KafkaTemplate<String,String> kafkaTemplate;
+    private final KafkaTemplate<String, String> kafkaTemplate;
 
-    public void enviarMensagem(String mensagem){
-        MessageBuilder<String> stringMessageBuilder = MessageBuilder.withPayload(mensagem)
-                .setHeader(KafkaHeaders.TOPIC,topic)
-                .setHeader(KafkaHeaders.MESSAGE_KEY, UUID.randomUUID().toString());
+    private final ObjectMapper objectMapper;
+
+    public void enviarMensagem(MensagemDTO mensagem, NomeChat nomeChat) throws JsonProcessingException {
+        String mensagemStr = objectMapper.writeValueAsString(mensagem);
+
+        MessageBuilder<String> stringMessageBuilder = MessageBuilder.withPayload(mensagemStr)
+                .setHeader(KafkaHeaders.TOPIC, nomeChat.getTopico())
+                .setHeader(KafkaHeaders.MESSAGE_KEY, UUID.randomUUID().toString())
+                .setHeader(KafkaHeaders.PARTITION_ID,nomeChat.getParticao());
+
         Message<String> message = stringMessageBuilder.build();
-        kafkaTemplate.send(message);
+
+        ListenableFuture<SendResult<String, String>> enviadoParaTopico = kafkaTemplate.send(message);
+        enviadoParaTopico.addCallback(new ListenableFutureCallback<>() {
+            @Override
+            public void onFailure(Throwable ex) {
+                log.error("Erro ao enviar a mensagem! {}", mensagemStr, ex);
+            }
+
+            @Override
+            public void onSuccess(SendResult<String, String> result) {
+                log.info("Mensagem enviada! {}", mensagemStr);
+            }
+        });
     }
 }
